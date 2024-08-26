9700e50de949364e741d753a9e2a2e8eef07ea4f
Bagus Dermawan Mulya <bagusdermawanmulya27@gmail.com>
2024-08-26 16:29:37 +0700

    Tugas - Hari 6

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from "@/layout";
 export default function Home() {
   return (
     <>
-      <Layout metaTitle="Home" metaDescription="Welcome to our homepage">
+      <Layout metaTitle="Home" metaDescription="Ini merupakan halaman Home">
         <p className="text-lg p-4 font-bold"> Halaman Home </p>{" "}
       </Layout>{" "}
     </>
--- pages/profile.js
+++ pages/profile.js
@@ -3,7 +3,10 @@ import Layout from "@/layout";
 export default function Profile() {
   return (
     <>
-      <Layout metaTitle="Profile" metaDescription="Profile">
+      <Layout
+        metaTitle="Profile"
+        metaDescription="Ini merupakan halaman profile"
+      >
         <p className="text-lg p-4 font-bold"> Halaman Profile </p>{" "}
       </Layout>{" "}
     </>
