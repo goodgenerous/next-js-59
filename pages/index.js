065e08035872887e82c59b8e2529813165eb0ec4
Bagus Dermawan Mulya <bagusdermawanmulya27@gmail.com>
2024-08-30 00:48:09 +0700

    Tugas - Hari 9

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 import Layout from "@/layout";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((res) => console.log("response => ", res))
+      .catch((err) => console.error("error => ", err));
+  }, []);
+
   return (
     <>
       <Layout metaTitle="Home" metaDescription="Ini merupakan halaman Home">
